34971abab0b4edf591ffa4122fdd92c01e769d15
Neo-Getsu <110412780+Neo-Getsu@users.noreply.github.com>
2023-04-04 14:04:24 +0200

    Structure + mongoDB: ok

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -1,17 +1,26 @@
 import { BotEvent } from "../types";
-import { Events, Interaction } from "discord.js";
+import { Events, Interaction, EmbedBuilder,   } from "discord.js";
+ 
 
 const event: BotEvent = {
     name: Events.InteractionCreate,
     once: false,
-    async execute(interaction: Interaction) {
+    async execute(  interaction: Interaction) {
         if (!interaction.isChatInputCommand()) return;
+   
+        // console.log(interaction)
+        console.log
 
         const command = interaction.client.slashCommands.get(interaction.commandName);
 
         if (!command) return;
 
         await command.execute(interaction);
+
+    
+	
+		 
+	  
     },
 }
 
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { Client, Collection, GatewayIntentBits } from 'discord.js';
 import { SlashCommand } from './types';
 import { join } from 'path';
 import { readdirSync } from 'fs';
+import { connect } from 'mongoose'
 
 dotenv.config();
 
@@ -16,11 +17,17 @@ const client = new Client({
 });
 
 client.slashCommands = new Collection<string, SlashCommand>();
-
+ 
 const handlersDir = join(__dirname, "./handlers");
 
 readdirSync(handlersDir).forEach(handler => {
   require(`${handlersDir}/${handler}`)(client);
+});
+
+connect(process.env.MONGO_URI, {}).then(() => {
+  console.log("Connected to MongoDB");
+}).catch(err => {
+  console.log(err);
 });
 
 client.login(process.env.TOKEN);
